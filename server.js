971ab134707b15e49b5731f60f1919fb60f457f0
Kasun Dulara <kasundularaam@gmail.com>
2022-02-28 11:36:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.get("/", (req, res) => {
+  res.send("hey people");
+});
+
+const PORT = process.env.PORT || 3000;
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  socket.on("driver", (receivedLocation) => {
+    console.log(receivedLocation);
+    socket.emit("user", receivedLocation);
+  });
+});
+server.listen(PORT, "0.0.0.0", () => console.log("app started"));
